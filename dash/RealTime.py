ac28c2759589d289f4647c4930496534909f4378
Cristian Pardo <pardo9210@hotmail.com>
2019-11-16 17:06:29 -0500

    Reads2_Raw change its name to DataRaw

--- dash/RealTime.py
+++ dash/RealTime.py
@@ -65,15 +65,29 @@ for station in station:
 #eliminacion de campos no usados
 del df['metar']
 del df['wxcodes']
+del df['lon']
+del df['lat']
+del df['skyc2']
+del df['skyc3']
+del df['skyc4']
+del df['skyl2']
+del df['skyl3']
+del df['skyl4']
+del df['ice_accretion_1hr']
+del df['ice_accretion_3hr']
+del df['ice_accretion_5hr']
+del df['peak_wind_gust']
+del df['peak_wind_drct']
+del df['peak_wind_time']
 print(df.shape)
 
 #creacion del motor de base de datos en postgres
 engine = create_engine('postgresql://ds4a_18:ds4a2019@ds4a18.cmlpaj0d1yqv.us-east-2.rds.amazonaws.com:5432/Airports_ds4a')
 #subir DataFrame a la base de datos
-df.to_sql(name='reads2_raw', con=engine, if_exists = 'append', index=False, chunksize=10000)
+df.to_sql(name='DataRaw', con=engine, if_exists = 'append', index=False, chunksize=10000)
 #eliminacion de duplicados con el query. Esta como una funcion en la base de datos
 engine.execute('select delete_duplicates()')
 #eliminacion de datos nulos (donde VALID es nulo)
-engine.execute('delete from reads2_raw where valid is null')
+engine.execute('delete from DataRaw where valid is null')
 print('FINISH')
 
