873e9e4853cd6020ace622467ccc10535f06aa3d
Andres R <afrodp95@outlook.com>
2019-11-16 14:42:19 -0500

    data cleaning script v2

--- dash/0_data_cleaning.py
+++ dash/0_data_cleaning.py
@@ -1,18 +1,14 @@
+#####################
+# Importar librerías
 import pandas as pd 
 import numpy as np
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
 import plotly.graph_objects as go
-from dash.dependencies import Input, Output
 import datetime
-import dash_table
-
-
-##############
-### Import Raw Data
-
+import time
 from sqlalchemy import create_engine
+
+#####################
+### Fecth Raw data from AWS DB
 
 engine = create_engine('postgresql://ds4a_18:ds4a2019@ds4a18.cmlpaj0d1yqv.us-east-2.rds.amazonaws.com:5432/Airports_ds4a')
 df = pd.read_sql("SELECT * from reads2_raw", engine.connect(), parse_dates=('valid',))
@@ -20,20 +16,19 @@ df = df.dropna(subset=['valid'],axis=0)
 df['DateTime'] = df['valid'].apply(lambda x: datetime.datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S"))
 df.replace(['M',None],np.nan,inplace=True)
 
-print('Data Succesfully Fetched From AWS RDS',end="\n\n")
+print('Data Succesfully Fetched From AWS RDS')
+
 
 #####################
 #### Select Columns With Na % lower than 22% (previously selected)
 
-print('Selecting Columns')
-
 cols = ['id','station','DateTime', 'valid', 'lon', 'lat', 'tmpf', 'dwpf', 'relh', 'drct',
        'sknt', 'p01i', 'alti', 'vsby', 'skyc1', 'skyl1', 'feel']
-
 df = df[cols]
 
 ###################
 #### Convert to Numeric (in sql are all text)
+
 numeric_cols = ['lon','lat','tmpf', 'dwpf', 'relh', 'drct', 'sknt', 'alti', 'vsby', 'skyl1', 'feel']
 
 for col in numeric_cols:
@@ -48,7 +43,7 @@ print('Finding Missing Data Intervals')
 df['day_hour'] = df['DateTime'].apply(lambda x: datetime.datetime.strftime(x, "%Y-%m-%d %H"))
 df = df.groupby(['station', 'day_hour','lon','lat']).mean().reset_index() ## Se pierde p01i skyc1 porque son categóricas
 
-#### Crear las horas wue no tienen observaciones
+#### Crear las horas que no tienen observaciones
 df['day_hour'] = pd.to_datetime(df['day_hour'])
 minimo = df.day_hour.min()
 maximo = df.day_hour.max()
@@ -76,9 +71,10 @@ print(len(df2))
 
 
 # Pegarle la informacin
-df2 = pd.merge(df,df2, left_on =['station','day_hour','lat','lon'], right_on=['station','day_hour','lat','lon'], how = 'left')
+df2 = pd.merge(df2,df, left_on =['station','day_hour','lat','lon'], right_on=['station','day_hour','lat','lon'], how = 'left')
 df = df2.copy()
 del(df2)
+
 
 ########################################
 ##### Usar Imputacion Seleccionada #####
@@ -102,13 +98,20 @@ airports = df['station'].unique()
 
 ### Iniciar imputacion iterativa
 df_list = []
-
+df_empty = pd.DataFrame()
 for airport in airports:
     df2 = df[df['station']==airport].reset_index(drop=True)
-    df2_num = df[numeric_cols]
+    hora = df2.day_hour
+    lati = df2.lat
+    longi = df2.lon
+    df2_num = df2[numeric_cols]
     df2_imp = m_imputer.fit_transform(df2_num)
     df2_imp = pd.DataFrame(df2_imp,columns=numeric_cols)
-    df2_full = pd.concat([df[['station','day_hour','lon','lat']].copy(),df2_imp],axis=1)
+    df2_imp['day_hour'] = hora
+    df2_imp['station'] = airport
+    df2_imp['lat'] = lati
+    df2_imp['lon'] = longi
+    df2_full = pd.concat([df_empty,df2_imp],axis=1)
     print('Data for {} imputed'.format(airport))
     df_list.append(df2_full)
 
@@ -117,12 +120,13 @@ df = pd.concat(df_list,axis=0,ignore_index=True)
 #############################
 #### Exportar Solo Bogotá V1
 
-print('Selecting Bogotá V1')
-
-df = df[df['station']=='SKBO']
+#print('Selecting Bogotá V1')
+#df = df[df['station']=='SKBO']
 
 #############################
 #### Exportar Solo Ultimos 2 Meses de Info
+
+print('Selecting Only last Two Months of Info V1')
 
 df['day_hour'] = pd.to_datetime(df['day_hour'])
 
