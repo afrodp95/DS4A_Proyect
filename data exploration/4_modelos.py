69b66f88f12c64b8e31293d999769d1bb797f8ff
Andres R <afrodp95@outlook.com>
2019-11-18 21:11:34 -0500

    xgboost training added

--- data exploration/4_modelos.py
+++ data exploration/4_modelos.py
@@ -52,7 +52,7 @@ df_fin = df_fin.sort_values(by=['day_hour'],ascending=False).reset_index(drop=Tr
 df_fin.dropna(inplace=True)
 
 ## Extraer Año Mes Dia Hora de la fecha
-df_fin['year']=df_fin['day_hour'].dt.year
+#df_fin['year']=df_fin['day_hour'].dt.year
 df_fin['month']=df_fin['day_hour'].dt.month
 df_fin['day']=df_fin['day_hour'].dt.day 
 df_fin['hour']=df_fin['day_hour'].dt.hour
@@ -68,33 +68,125 @@ train = df_fin[df_fin['day_hour']<='2019-10-16']
 test = df_fin[df_fin['day_hour']>'2019-10-16']
 
 
-##########
-### Iniciar con un Random Forest
+#######################
+### RANDOM FOREST #####
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import auc, mean_squared_error, mean_absolute_error
 from sklearn.model_selection import GridSearchCV
 
 ### Matriz X & y de entrenamiento
-
 X_train = train.drop(['day_hour','vsby'],axis=1)
 Y_train = train['vsby']
 
 #### Parametrizacion del Modelo y Grilla de Validacion Cruzada
 estim = RandomForestRegressor(n_estimators=100,criterion='mse',n_jobs=1,random_state=123)
-forest = GridSearchCV(estimator=estim,cv=5,param_grid={'max_depth':np.arange(X_train.shape[1])+1},verbose=2,random_state=123)
+forest = GridSearchCV(estimator=estim,cv=5,param_grid={'max_depth':np.arange(X_train.shape[1])+1},verbose=2)
 
 ##### Entrenar el modelo sobre la grilla de validacion
 forest.fit(X=X_train,y=Y_train)
 
-##### Matriz Y de test
+##### Matriz X & Y de test
 Y_test = test['vsby']
+X_test = test.drop(['day_hour','vsby'],axis=1)
 
 ##### Mejor Parámetro
 forest.best_params_
+### {'max_depth': 8}
+
 
 #### Mejor Error Cuadratico Medio
 forest.best_score_
+## 0.23321001075392683
+
+#### Prediccion
+Y_pred = forest.predict(X_test)
+
+#### MSE de test
+test_mse = mean_squared_error(Y_test,Y_pred)
+
+#### Gráfico Predicción
+plt.subplots(figsize=(12,6))
+plt.plot(test['day_hour'],Y_test,label="Real")
+plt.plot(test['day_hour'],Y_pred,label="Predicted")
+plt.legend()
+plt.xlabel('Date')
+plt.ylabel('Horizontal Visibility')
+plt.xticks(rotation=30)
+plt.title("100 Trees 8 Max Depth Random Forest \n Training MSE: {:0.3f} Test MSE: {:0.3f}".format(forest.best_score_,test_mse))
+plt.show()
+
+
+#### Importancias Relativas
+forest.feature_importances_
+
+
+#################
+### XGBOOST #####
+
+from xgboost import XGBRegressor
+from sklearn.metrics import auc, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint
+
+
+### Matriz X & y de entrenamiento
+X_train = train.drop(['day_hour','vsby'],axis=1)
+Y_train = train['vsby']
+
+#### Parametrizacion del Modelo y Grilla de Validacion Cruzada
+estim = XGBRegressor(booster='gbtree',verbosity=0,random_state=123)
+np.random.seed(1234)
+param_dist = {"max_depth": np.linspace(4,10,num=6,dtype='int'),
+              "eta":np.random.uniform(0,1,size=20),
+              "subsample":np.random.uniform(0,1,size=20),
+              "max_features": randint(1, 11),
+              "min_samples_split": randint(2, 11),
+              "bootstrap": [True, False],
+              "criterion": ["gini", "entropy"]}
+
+xgb = RandomizedSearchCV(estimator=estim,cv=5,param_distributions=param_dist,verbose=2)
+
+##### Entrenar el modelo sobre la grilla de validacion
+xgb.fit(X=X_train,y=Y_train)
+
+##### Matriz X & Y de test
+Y_test = test['vsby']
+X_test = test.drop(['day_hour','vsby'],axis=1)
+
+##### Mejor Parámetro
+forest.best_params_
+### {'max_depth': 8}
+
+
+#### Mejor Error Cuadratico Medio
+forest.best_score_
+## 0.23321001075392683
+
+#### Prediccion
+Y_pred = forest.predict(X_test)
+
+#### MSE de test
+test_mse = mean_squared_error(Y_test,Y_pred)
+
+#### Gráfico Predicción
+plt.subplots(figsize=(12,6))
+plt.plot(test['day_hour'],Y_test,label="Real")
+plt.plot(test['day_hour'],Y_pred,label="Predicted")
+plt.legend()
+plt.xlabel('Date')
+plt.ylabel('Horizontal Visibility')
+plt.xticks(rotation=30)
+plt.title("100 Trees 8 Max Depth Random Forest \n Training MSE: {:0.3f} Test MSE: {:0.3f}".format(forest.best_score_,test_mse))
+plt.show()
+
+
+#### Importancias Relativas
+forest.feature_importances_
+
+
+
+
 
 
 ### Grafico
